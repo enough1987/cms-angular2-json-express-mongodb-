22640d30b7169b5a392e0b5ef117c6c97375a913
enough1987 <tilgaaleksandr@meta.ua>
2016-09-08 14:26:00 +0300

    go

--- assets/app/components/app.component.ts
+++ assets/app/components/app.component.ts
@@ -14,12 +14,13 @@ import {ObjectToArrayPipe} from "../pipes/objectToArray.pipe";
 export class AppComponent {
     public new_component = this.init_new_component();
     public components = [];
+    public field_editable = [];
 
     constructor(public storageService: StorageService) {}
 
     ngOnInit() {
         console.log('ngOnInit');
-        //this.get_all_components();
+        this.get_all_components();
     };
 
     init_new_component(mutability = ''){
@@ -30,6 +31,15 @@ export class AppComponent {
 
     set_new_component_mutability(mutability){
         this.new_component =  this.init_new_component( mutability );
+    };
+
+
+    get_all_components() {
+        this.storageService.select('/api/components' ).
+            subscribe( res => {
+                console.log( 'get - ' , res );
+                if( !res.error ) this.components = res.components;
+            });
     };
 
     create_component(){
@@ -122,191 +132,19 @@ export class AppComponent {
         return false;
     };
 
+    edit_fields_component(component){
+        this.field_editable = component;
+    };
+
+    show_fields_component(component){
+        console.log( this.field_editable , component );
+        if (  this.field_editable &&
+              this.field_editable._id === component._id 
+        ) return true;
+    }; 
 
 
     
-
-/*
-        public create_component = {};
-    public new_field = {};
-
-    get_all_components() {
-        this.storageService.select('/api/components' ).
-            subscribe( res => {
-                console.log( 'get - ' , res );
-                if( !res.error ) this.components = res.components;
-            });
-    };
-
-    create_component_func() {
-        if ( !this.create_component.name ) {
-            this.create_component.response = 'no data was provided '
-            return;
-        }
-        if( this.check_if_component_exist(this.create_component.name) ){
-            console.log(' this name exists ');
-            return;
-        }
-        this.storageService.insert('/api/components',{
-            name : this.create_component.name,
-            group : this.create_component.group,
-            body : []
-         }).
-            subscribe( res => {
-                console.log( 'post - ' , res );
-                this.create_component.response = res.msg;
-                if ( !res.error ) this.components = res.components;
-            });
-    };
-
-    change_component(item){
-        //console.log( item );
-        if ( !item.new_name ) {
-            console.log('no name was provided ');
-            return;
-        }
-        if( this.check_if_component_exist(item.new_name) ){
-            console.log(' this name exists ');
-            return;
-        }
-        this.storageService.update('/api/components',{
-            id : item._id,
-            name : item.new_name,
-            group : item.group,
-            body : item.body
-        }).
-            subscribe( res => {
-                console.log( 'put - ' , res );
-                if ( !res.error ) this.components = res.components;
-        });
-    };
-
-    copy_component(item) {
-        if ( !item.new_name ) {
-            console.log('no name was provided ');
-            return;
-        }
-        if( this.check_if_component_exist(item.new_name) ){
-            console.log(' this name exists ');
-            return;
-        }
-        this.storageService.insert('/api/components',{
-            name : item.new_name,
-            group :item.group,
-            body : item.body
-         }).
-            subscribe( res => {
-                console.log( 'post - ' , res );
-                this.create_component.response = res.msg;
-                if ( !res.error ) this.components = res.components;
-         });
-    };
-
-    delete_component(id) {
-        if ( !id ) {
-            console.log('no id was provided ');
-            return;
-        }
-        this.storageService.delete('/api/components', id).
-            subscribe( res => {
-                console.log( 'delete - ' , res );
-                if ( !res.error ) this.components = res.components;
-            });
-    };
-
-   check_if_component_exist(component_name){
-       let exist = false;
-       this.components.map(item => { item.name === component_name ? exist = true : false });
-       return exist;
-   };
-
-    show_single_component(item){
-        this.single_component = item;
-    };
-
-    change_new_field_type(){
-        this.new_field.value = '';
-    };
-
-    add_new_field( ) {
-        if( !this.new_field.name || this.check_if_field_exist(this.new_field.name ) ){
-            console.log(' this name is not OK ');
-            return;
-        }
-        if ( this.new_field.type == 'number' ) {
-            this.new_field.value ? this.new_field.value = Number(this.new_field.value) : '';
-        }
-        if ( this.new_field.type == 'boolean' ) {
-            this.new_field.value = !!this.new_field.value;
-        }
-        this.new_field.id = this.create_guid();
-        let new_field  = this.deepCopy(this.new_field);
-        this.single_component.body = [...this.single_component.body, new_field];
-        this.update_component();
-    };
-
-   check_if_field_exist(field_name){
-       let exist = false;
-       this.single_component.body.map(item => { item.name == field_name ? exist = true : false });
-       return exist;
-   };
-
-   change_field_func(item, deep) {
-       if ( deep ) {
-           console.log( 'deep' );
-       } 
-       let update =  true;
-       this.single_component.body.map(el => {
-           if ( el.name == item.name && el.id != item.id) {
-               update =  false;
-               //console.log(el.id +' != '+ item.id)
-           }
-       });
-       if ( update ) this.update_component();
-
-   };
-
-   delete_field_func(item, deep){
-       if ( deep ) {
-           console.log( 'deep' );
-       } 
-       this.single_component.body.map( (el, i) => {
-           if ( el.id == item.id ) {
-               this.single_component.body.splice(i, 1);
-               this.update_component();
-           }
-       });       
-   };
-
-   disabled_if_false(type) {
-       //console.log( !type );
-       return !type;
-   };
-
-    update_component() {
-         this.storageService.update('/api/components', {
-            id : this.single_component._id,
-            name : this.single_component.name,
-            body : this.single_component.body
-        }).
-            subscribe( res => {
-                console.log( 'put - ' , res );
-                if( !res.error ) {
-                    this.components = res.components;
-                    this.components.map( (el, i) => {
-                        if ( this.components[i]._id == this.single_component._id ){
-                            this.single_component = this.components[i];
-                        }
-                    });
-                }
-            });
-    };
-
-
-
-
-
-    */
 
     // return unique id
     create_guid() {
@@ -333,38 +171,5 @@ export class AppComponent {
 
 
 
-
-    /*
-    insert_func() {
-        this.storageService.insert('/api/pages',{
-            name : this.create_page.name,
-            body : []
-         }).
-            subscribe( res => {
-                console.log( 'post - ' , res );
-                this.insert =  JSON.stringify(res);
-            });
-    };
-
-    update_func() {
-        this.storageService.update('/api/pages',{
-            id : this.update_id,
-            name : ' header ',
-            body : [{ name: 'test', type: 'String'}]
-        }).
-            subscribe( res => {
-                console.log( 'put- ' , res );
-                this.update =  JSON.stringify(res);
-            });
-    };
-
-    delete_func() {
-        this.storageService.delete('/api/pages', this.delete_id).
-            subscribe( res => {
-                console.log( 'delete- ' , res );
-                this.delete =  JSON.stringify(res);
-            });
-    };
-    */
 
 }
